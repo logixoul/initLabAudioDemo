d2088e64422f76ead03a8d74e9b7c4b35421fa76
Stefan Monov <logixoul@gmail.com>
2024-09-21 20:03:13 +0300

    simplified

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-var context = new AudioContext();
+let context = new AudioContext();
 
-function playSound(arr) {
-  var buf = new Float32Array(arr.length)
-  for (var i = 0; i < arr.length; i++) buf[i] = arr[i]
-  var buffer = context.createBuffer(1, buf.length, context.sampleRate)
-  buffer.copyToChannel(buf, 0)
-  var source = context.createBufferSource();
-  source.buffer = buffer;
-  source.connect(context.destination);
-  source.start(0);
+function playSound() {
+    let buf = new Float32Array(context.sampleRate * seconds)
+    for (let i = 0; i < buf.length; i++)
+        buf[i] = sineWaveAt(i, tone) * volume;
+    let buffer = context.createBuffer(1, buf.length, context.sampleRate)
+    buffer.copyToChannel(buf, 0)
+    let source = context.createBufferSource();
+    source.buffer = buffer;
+    source.connect(context.destination);
+    source.start(0);
 }
 
 function sineWaveAt(sampleNumber, tone) {
-  var sampleFreq = context.sampleRate / tone
+  const sampleFreq = context.sampleRate / tone
   return Math.sin(sampleNumber / (sampleFreq / (Math.PI * 2)))
 }
 
-var arr = [],
-  volume = 0.2,
+let volume = 0.2,
   seconds = 0.5,
   tone = 441
 
-for (var i = 0; i < context.sampleRate * seconds; i++) {
-  arr[i] = sineWaveAt(i, tone) * volume
-}
-
 document.addEventListener("keydown", () => {
-    playSound(arr);
+    playSound();
     console.log("playing")
 });
