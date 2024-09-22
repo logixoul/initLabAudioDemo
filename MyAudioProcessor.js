bfb2c08c1bad4143131e95424255eb092fcabd55
Stefan Monov <logixoul@gmail.com>
2024-09-22 09:34:09 +0300

    currently works

--- MyAudioProcessor.js
+++ MyAudioProcessor.js
@@ -1,21 +1,19 @@
 class MyAudioProcessor extends AudioWorkletProcessor {
     constructor() {
         super();
-        this.frequency = 440; // Default frequency is 440Hz (A4 note)
         this.phase = 0;
-        this.sampleRate = sampleRate; // Access the sample rate of the audio context
     }
 
     process(inputs, outputs, parameters) {
         const output = outputs[0];
-        const sampleRate = this.sampleRate;
+        const frequency = 440;
 
         for (let channel = 0; channel < output.length; channel++) {
             const outputChannel = output[channel];
 
             for (let i = 0; i < outputChannel.length; i++) {
                 outputChannel[i] = Math.sin(this.phase);
-                this.phase += (2 * Math.PI * this.frequency) / sampleRate;
+                this.phase += (2 * Math.PI * frequency) / sampleRate;
             }
         }
 
---
+++ SineOscillator.js
@@ -0,0 +1,5 @@
+export class SineOscillator {
+    constructor() {
+        self.frequency = 440;
+    }
+}
