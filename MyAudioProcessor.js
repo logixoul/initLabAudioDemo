f5d8d27a980f73900ac8c4a527c8936e8c515cb8
Stefan Monov <logixoul@gmail.com>
2024-09-22 10:21:58 +0300

    basic envelope

--- MyAudioProcessor.js
+++ MyAudioProcessor.js
@@ -6,17 +6,20 @@ class MyAudioProcessor extends AudioWorkletProcessor {
         super();
         this.osc = new SineOscillator(sampleRate);
         //this.phase = 0;
+        this.time = 0;
     }
 
     process(inputs, outputs, parameters) {
         const outputChannels = outputs[0];
-
+        //if(currentTime > 3)
+        //    return true;
         for (let i = 0; i < outputChannels[0].length; i++) {
             let sample = this.osc.nextSample();
+            this.time++;
+            sample *= Math.pow(0.999, this.time);
             for (let channel = 0; channel < outputChannels.length; channel++) {
                 const outputChannel = outputChannels[channel];
                 outputChannel[i] = sample;
-                //outputChannel[i] = Math.sin(i);
             }
         }
     
