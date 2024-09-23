8f1de762f0937ed39d381cc0300f80cc07ea4454
Stefan Monov <logixoul@gmail.com>
2024-09-23 13:06:33 +0300

    refactoring for oscillator selection (not finished)

--- main.js
+++ main.js
@@ -2,64 +2,71 @@ import { AudioThreadManager } from "./AudioThreadManager.js";
 import { SineOscillator } from "./SineOscillator.js";
 import { SquareOscillator } from "./SquareOscillator.js";
 
-function characterToNoteIndex(char) {
-    const mapping = {
-        z: 0,
-        s: 1,
-        x: 2,
-        d: 3,
-        c: 4,
-        v: 5,
-        g: 6,
-        b: 7,
-        h: 8,
-        n: 9,
-        j: 10,
-        m: 11        
-    };
-    if(char in mapping) {
-        return mapping[char];
-    }
-    return null;
-}
-
 class Configuration {
     sineShaping = 1;
     oscillatorClass = "SineOscillator";
 }
 
-export function main() {
-    let audioThreadRunning = false;
-    let audioThreadManager = new AudioThreadManager();
-    let configuration = new Configuration();
-    
-    document.getElementById("shapingSlider").onchange = (e) => {
-        configuration.sineShaping = e.target.value
-        audioThreadManager.postMessage({
-            name: "setConfiguration",
-            value: configuration
-        });
-    };
+export class App {
+    audioThreadRunning = false;
+    audioThreadManager = new AudioThreadManager();
+    configuration = new Configuration();
 
-    document.addEventListener("keydown", async (e) => {
-        if(!audioThreadRunning) {
-            await audioThreadManager.launchThread();
-            //let sineOscFactory = (sampleRate, frequency) => new SineOscillator(sampleRate, frequency, 1);
-            audioThreadManager.postMessage({
+    constructor() {
+        document.getElementById("shapingSlider").onchange = (e) => {
+            this.configuration.sineShaping = e.target.value
+            this.audioThreadManager.postMessage({
                 name: "setConfiguration",
-                value: configuration
+                value: this.configuration
             });
-        
-            audioThreadRunning = true;
-            console.log("launched")
+        };
+
+        document.addEventListener("keydown", async (e) => {
+            if(!this.audioThreadRunning) {
+                await this.audioThreadManager.launchThread();
+                //let sineOscFactory = (sampleRate, frequency) => new SineOscillator(sampleRate, frequency, 1);
+                this.audioThreadManager.postMessage({
+                    name: "setConfiguration",
+                    value: this.configuration
+                });
+            
+                this.audioThreadRunning = true;
+                console.log("launched")
+            }
+            const noteIndex = this.characterToNoteIndex(e.key)
+            if(noteIndex != null) {
+                const powerBase = Math.pow(2, 1/12);
+                this.audioThreadManager.postMessage({
+                    name: "playNote",
+                    frequency: 440 * Math.pow(powerBase, noteIndex)
+                });
+            }
+        });
+    }
+
+    characterToNoteIndex(char) {
+        const mapping = {
+            z: 0,
+            s: 1,
+            x: 2,
+            d: 3,
+            c: 4,
+            v: 5,
+            g: 6,
+            b: 7,
+            h: 8,
+            n: 9,
+            j: 10,
+            m: 11        
+        };
+        if(char in mapping) {
+            return mapping[char];
         }
-        const noteIndex = characterToNoteIndex(e.key)
-        if(noteIndex != null) {
-            const powerBase = Math.pow(2, 1/12);
-            audioThreadManager.postMessage({
-                name: "playNote",
-                frequency: 440 * Math.pow(powerBase, noteIndex)
-            });
-        }
-    });
+        return null;
+    }
+}
+
+export function main() {
+    
+
 }
