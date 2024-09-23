b237deedcebea0b89ecd2df36b2fb2ecab823081
Stefan Monov <logixoul@gmail.com>
2024-09-23 20:13:01 +0300

    minor refactoring and cleanup

--- MyAudioProcessor.js
+++ MyAudioProcessor.js
@@ -34,17 +34,22 @@ class MyAudioProcessor extends AudioWorkletProcessor {
         };
     }
 
+    nextSample() {
+        let sample = 0;
+        for(const note of this.notes)
+            sample += note.nextSample();
+        sample = this.filter.processSample(sample);
+        sample = this.echo.processSample(sample);
+        sample *= .4;
+        return sample;
+    }
+
     process(inputs, outputs, parameters) {
         this.removeFinishedNotes();
 
         const outputChannels = outputs[0];
         for (let i = 0; i < outputChannels[0].length; i++) {
-            let sample = 0;
-            for(const note of this.notes)
-                sample += note.nextSample();
-            sample = this.filter.processSample(sample);
-            sample = this.echo.processSample(sample);
-            sample *= .4;
+            let sample = this.nextSample();
             for (let channel = 0; channel < outputChannels.length; channel++) {
                 const outputChannel = outputChannels[channel];
                 outputChannel[i] = sample;
