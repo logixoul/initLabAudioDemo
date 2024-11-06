2cd560392cb34c2f3c9b37903ec435696c752a64
Vencislav Atanasov <git@6bez10.info>
2024-11-06 23:26:48 +0200

    Export all constants

--- Midi.js
+++ Midi.js
@@ -1,13 +1,13 @@
 // channel voice messages
-const NOTE_OFF = 0x08;
-const NOTE_ON = 0x09;
-const POLYPHONIC_KEY_PRESSURE = 0x0A;
-const CONTROL_CHANGE = 0x0B;
-const PROGRAM_CHANGE = 0x0C;
-const CHANNEL_PRESSURE = 0x0D;
-const PITCH_BEND_CHANGE = 0x0E;
+export const NOTE_OFF = 0x08;
+export const NOTE_ON = 0x09;
+export const POLYPHONIC_KEY_PRESSURE = 0x0A;
+export const CONTROL_CHANGE = 0x0B;
+export const PROGRAM_CHANGE = 0x0C;
+export const CHANNEL_PRESSURE = 0x0D;
+export const PITCH_BEND_CHANGE = 0x0E;
 
-const CHANNEL_VOICE_MESSAGE_TYPES = {
+export const CHANNEL_VOICE_MESSAGE_TYPES = {
     [NOTE_OFF]: 'noteoff',
     [NOTE_ON]: 'noteon',
     [POLYPHONIC_KEY_PRESSURE]: 'polyaftertouch',
@@ -18,16 +18,16 @@ const CHANNEL_VOICE_MESSAGE_TYPES = {
 };
 
 // channel mode messages
-const ALL_SOUND_OFF = 0x78;
-const RESET_ALL_CONTROLLERS = 0x79;
-const LOCAL_CONTROL = 0x7A;
-const ALL_NOTES_OFF = 0x7B;
-const OMNI_MODE_OFF = 0x7C;
-const OMNI_MODE_ON = 0x7D;
-const MONO_MODE_ON = 0x7E;
-const POLY_MODE_ON = 0x7F;
+export const ALL_SOUND_OFF = 0x78;
+export const RESET_ALL_CONTROLLERS = 0x79;
+export const LOCAL_CONTROL = 0x7A;
+export const ALL_NOTES_OFF = 0x7B;
+export const OMNI_MODE_OFF = 0x7C;
+export const OMNI_MODE_ON = 0x7D;
+export const MONO_MODE_ON = 0x7E;
+export const POLY_MODE_ON = 0x7F;
 
-const CHANNEL_MODE_MESSAGE_TYPES = {
+export const CHANNEL_MODE_MESSAGE_TYPES = {
     [ALL_SOUND_OFF]: 'allsoundoff',
     [RESET_ALL_CONTROLLERS]: 'resetallcontrollers',
     [LOCAL_CONTROL]: 'localcontrol',
@@ -39,26 +39,26 @@ const CHANNEL_MODE_MESSAGE_TYPES = {
 };
 
 // system common messages
-const SYSTEM_EXCLUSIVE = 0xF0;
-const MIDI_TIME_CODE_QUARTER_FRAME = 0xF1;
-const SONG_POSITION_POINTER = 0xF2;
-const SONG_SELECT = 0xF3;
-const SYSTEM_RESERVED_1 = 0xF4;
-const SYSTEM_RESERVED_2 = 0xF5;
-const TUNE_REQUEST = 0xF6;
-const END_OF_EXCLUSIVE = 0xF7;
+export const SYSTEM_EXCLUSIVE = 0xF0;
+export const MIDI_TIME_CODE_QUARTER_FRAME = 0xF1;
+export const SONG_POSITION_POINTER = 0xF2;
+export const SONG_SELECT = 0xF3;
+export const SYSTEM_RESERVED_1 = 0xF4;
+export const SYSTEM_RESERVED_2 = 0xF5;
+export const TUNE_REQUEST = 0xF6;
+export const END_OF_EXCLUSIVE = 0xF7;
 
 // system real-time messages
-const TIMING_CLOCK = 0xF8;
-const SYSTEM_RESERVED_3 = 0xF9;
-const START = 0xFA;
-const CONTINUE = 0xFB;
-const STOP = 0xFC;
-const SYSTEM_RESERVED_4 = 0xFD;
-const ACTIVE_SENSING = 0xFE;
-const RESET = 0xFF;
+export const TIMING_CLOCK = 0xF8;
+export const SYSTEM_RESERVED_3 = 0xF9;
+export const START = 0xFA;
+export const CONTINUE = 0xFB;
+export const STOP = 0xFC;
+export const SYSTEM_RESERVED_4 = 0xFD;
+export const ACTIVE_SENSING = 0xFE;
+export const RESET = 0xFF;
 
-const SYSTEM_MESSAGE_TYPES = {
+export const SYSTEM_MESSAGE_TYPES = {
     // common
     [SYSTEM_EXCLUSIVE]: 'sysex',
     [MIDI_TIME_CODE_QUARTER_FRAME]: 'mtc',
